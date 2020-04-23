29c9b2d7e8b20510493f9801c105b837f1eeaaf0
Erick <erick.munekata@gmail.com>
2020-04-23 18:29:54 -0300

    Conceitos de Node.js com TypeScript

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type.toLowerCase() !== 'income' && type.toLowerCase() !== 'outcome') {
+      throw Error('This is an invalid transaction type');
+    }
+
+    if (value < 0) {
+      throw Error(`Negative values aren't accepted.`);
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('The outcome value cannot be greater than total balance.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
